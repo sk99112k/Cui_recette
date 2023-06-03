b69c60acd6a9c267829523630274914c80e0e425
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-03 11:30:30 +0000

    edit 変更

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -11,15 +11,6 @@ class Public::MembersController < ApplicationController
   def edit
     @member = current_member
   end
-
-  def update
-    @member = current_member
-    if @member.update(member_params)
-      redirect_to member_path(@member)
-    else
-      render "edit"
-    end
-  end
   
   private
   
