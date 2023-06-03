f4afad99efe57636c6182cf596e90b85bbdee02c
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-03 17:06:42 +0000

    Loginできない

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-  
+  before_action :configure_sign_in_params, only: [:create]
+
   # sign_in後ユーザー詳細ページに遷移
   def after_sign_up_path_for(resource)
     super(resource)
@@ -27,7 +27,7 @@ class Public::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name, :password])
+  end
 end
