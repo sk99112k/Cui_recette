faaf6f0997ae73e9e7c0be6b9a87557916867441
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-04 09:22:28 +0000

    スライドショー helper使用

--- app/helpers/public/homes_helper.rb
+++ app/helpers/public/homes_helper.rb
@@ -1,2 +1,12 @@
 module Public::HomesHelper
+  def top_slider_alt_datas
+    { 1 => "aaaの画像",
+      2 => "bbbの画像",
+      3 => "cccの画像",
+      4 => "dddの画像",
+      5 => "eeeの画像",
+      6 => "fffの画像"
+    }
+
+  end
 end
