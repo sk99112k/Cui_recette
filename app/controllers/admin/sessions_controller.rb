badae06602708da60e9035739de9cf8d0ab20c2e
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-05 03:15:46 +0000

    admin topページ

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,6 +3,14 @@
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  def after_sign_in_path_for(resource)
+    admin_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -21,7 +29,7 @@ class Admin::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:password, :email])
+  end
 end
