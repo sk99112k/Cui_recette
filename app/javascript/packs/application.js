1479c33ab853a14a8a0852f807453ce0ffff3fc7
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-06 03:36:41 +0000

    field 追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,11 +11,7 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"; 
-import "recipe.js";
-import "../stylesheets/public/recipe";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-console.log("test")
