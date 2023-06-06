e6e88d94537953e20f42b6ef20803f658131307e
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-06 11:04:31 +0000

    admin ログイン

--- app/controllers/admin/parameter_sanitizer.rb
+++
@@ -1,6 +0,0 @@
-class Admin::ParameterSanitizer < Devise::ParameterSanitizer
-  def initialize(*)
-    super
-    permit(:sign_in, keys: [:password, :email] )
-  end
-end
--- app/controllers/public/members_parameter_sanitizer.rb
+++
@@ -1,6 +0,0 @@
-class Public::MembersParameterSanitizer < Devise::ParameterSanitizer
-  def initialize(*)
-    super
-    permit(:sign_in, keys: [:password, :name] )
-  end
-end
