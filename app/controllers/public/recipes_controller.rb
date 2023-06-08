f6f24c2431da2ddceeca00e9a89b6d34e2ff802d
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-08 05:58:34 +0000

    quantity　作成中

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -18,9 +18,13 @@ class Public::RecipesController < ApplicationController
     # 中間テーブルのListStoragに保存される
     # 追加した分のlist_idを繰り返しで保存する
     if @recipe.save!
-      params[:lists].each do |list|
-        ListStorage.create(recipe_id: @recipe.id, list_id: list)
+      quantities = params[:quantities] || {}
+      quantities.each do |list_id, quantity|
+        ListStorage.create(recipe_id: @recipe.id, list_id: list_id, quantity: quantity)
       end
+      # params[:quantities][:lists].each do |list|
+      #   ListStorage.create(recipe_id: @recipe.id, list_id: list, quantity: quantity)
+      # end
       redirect_to recipes_path
     else
       render "new"
@@ -28,19 +32,23 @@ class Public::RecipesController < ApplicationController
   end
 
   def edit
+    @recipe = Recipe.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
   end
 
   private
 
   def recipe_params
     # params.require(:recipe).permit(:title, :body, :genre, :list_id=>, :image, :quantity)
-    params.require(:recipe).permit(:title, :body, :genre, :image)
+    params.require(:recipe).permit(:title, :body, :genre, :image, list_attributes: :quantity)
   end
 
 end
