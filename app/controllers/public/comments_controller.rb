09a7f2aab373c0ca98dbaeb7220c56a1cc933a90
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-09 09:25:33 +0000

    非同期通信

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -5,7 +5,7 @@ class Public::CommentsController < Public::ApplicationController
     @comment = current_member.comments.new(comment_params)
     @comment.recipe_id = @recipe.id
     @comment.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
   
   # def destroy
