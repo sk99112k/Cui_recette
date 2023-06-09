ce1c618184aba40c2f60c24f8a09643e577831d1
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-09 09:09:50 +0000

    非同期通信

--- app/controllers/public/book_marks_controller.rb
+++ app/controllers/public/book_marks_controller.rb
@@ -4,14 +4,14 @@ class Public::BookMarksController < Public::ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @book_mark = current_member.book_marks.new(recipe_id: @recipe.id)
     @book_mark.save
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
   def destroy
     @recipe = Recipe.find(params[:recipe_id])
     @book_mark = current_member.book_marks.find_by(recipe_id: @recipe.id)
     @book_mark.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
   end
 
 end
