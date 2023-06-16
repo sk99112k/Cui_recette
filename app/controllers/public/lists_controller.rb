ee695014c37472062431be5fd94b61e884f56e6d
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-16 04:44:57 +0000

    pagination

--- app/controllers/public/lists_controller.rb
+++ app/controllers/public/lists_controller.rb
@@ -14,7 +14,7 @@ class Public::ListsController < Public::ApplicationController
   end
 
   def index
-    @lists = List.order("name_kana")
+    @lists = List.order("name_kana").all.page(params[:page]).per(5)
   end
 
   def show
--- app/models/list.rb
+++ app/models/list.rb
@@ -5,11 +5,11 @@ class List < ApplicationRecord
 
   # バリデーション
   with_options presence: true do
-    validates :name
+    validates :name, presence: true
     validates :supplier
     validates :unit
     validates :name_kana, format: { with: /\A[ｧ-ﾝﾞﾟ]+\z/, message: 'は半角ｶﾀｶﾅで入力して下さい'}
-    # "numericality"オプション. "allow_blank: true"空欄の場合にはバリデーションをスキップ  
+    # "numericality"オプション. "allow_blank: true"空欄の場合にはバリデーションをスキップ
     validates :lot, numericality: { allow_blank: true, message: 'は数値で入力して下さい' }
     # "only_integer: true"小数点以下の値を受け入れない
     validates :price, numericality: { only_integer: true, allow_blank: true, message: 'は半角の整数値で入力して下さい' }
