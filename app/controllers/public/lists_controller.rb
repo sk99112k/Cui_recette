ddb3746d71be2b489afd3a6745be806cebbb6956
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-17 00:50:05 +0000

    List編集

--- app/controllers/public/lists_controller.rb
+++ app/controllers/public/lists_controller.rb
@@ -14,7 +14,7 @@ class Public::ListsController < Public::ApplicationController
   end
 
   def index
-    @lists = List.order("name_kana").all.page(params[:page]).per(5)
+    @lists = List.order("name_kana").page(params[:page]).per(5)
   end
 
   def show
