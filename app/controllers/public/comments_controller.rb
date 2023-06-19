765d22b1b856e1789142f2e2acb4d45da9a9c4e7
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-19 10:42:21 +0000

    レイアウト修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,5 +1,5 @@
 class Public::CommentsController < Public::ApplicationController
-  
+
   def create
     @recipe = Recipe.find(params[:recipe_id])
     @comment = current_member.comments.new(comment_params)
@@ -7,18 +7,18 @@ class Public::CommentsController < Public::ApplicationController
     @comment.save
     # redirect_to request.referer
   end
-  
+
   def destroy
     @recipe = Recipe.find(params[:recipe_id])
-    @comment = current_member.comments.find_by(recipe_id: @recipe.id)
+    @comment = Comment.find(params[:id])
     @comment.destroy
     # redirect_to request.referer
-  end 
-  
+  end
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:body)
-  end 
-  
+  end
+
 end
