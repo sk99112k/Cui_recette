65c4272eee5521c6d035a21f5364cad1e4b9e6a2
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-19 09:09:25 +0000

    ページネーション

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -2,7 +2,7 @@ class Admin::MembersController < Admin::ApplicationController
   
   def show
     @member = Member.find(params[:id])
-    @recipes = @member.recipes
+    @recipes = @member.recipes.page(params[:page]).per(10)
   end
   
 end
