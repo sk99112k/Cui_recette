ff32a28f86623d4baf538040d603dabd25d47384
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-23 04:05:18 +0000

    admin guest修正

--- app/controllers/admin/guests_controller.rb
+++ app/controllers/admin/guests_controller.rb
@@ -1,12 +1,2 @@
 class Admin::GuestsController < ApplicationController
-  before_action :new_guest, only: [:edit, :destroy]
-
-  def new_guest
-    admin = Admin.find_or_create_by!(email: 'guest@example.com') do |admin|
-    admin.password = SecureRandom.urlsafe_base64
-    end
-    sign_in(admin)
-    redirect_to admiCn_root_path, notice: 'ゲストユーザーとしてログインしました。'
-  end
-
 end
