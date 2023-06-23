83b93a69339c3eda8dc116cae50c5954dc139f5b
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-23 06:55:02 +0000

    レイアウト編集

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -2,7 +2,7 @@ class Public::GenresController < Public::ApplicationController
 
   def search
     @genre = Genre.find(params[:genre_id])
-    @genres = @genre.recipes.page(params[:page]).per(8)
+    @genres = @genre.recipes.page(params[:page]).per(12)
   end
 
 end
