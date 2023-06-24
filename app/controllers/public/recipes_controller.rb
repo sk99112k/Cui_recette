91c698343360539bc9cfe345cf8fa7c0a13622db
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-24 13:16:12 +0000

    recipe new 修正

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -17,6 +17,7 @@ class Public::RecipesController < Public::ApplicationController
     @list = List.new
   end
 
+
   def create
     @recipe = Recipe.new(recipe_params)
 
@@ -25,9 +26,8 @@ class Public::RecipesController < Public::ApplicationController
       key = recipe_params[:list_storages_attributes].keys.first
       list_storage_params = recipe_params[:list_storages_attributes][key]
       list_storage_params.delete('_destroy')
-      # list_storage_first = ListStorage.new(list_storage_params)
       #formはあるが、入力されていない時
-      is_list_storage_valid = ListStorage.new(list_storage_params).valid?
+      # is_list_storage_valid = @recipe.list_storages.new(list_storage_params).valid?
       first_list_id = recipe_params[:list_storages_attributes][key]["list_id"]
     else
       #formがない時
@@ -36,16 +36,18 @@ class Public::RecipesController < Public::ApplicationController
 
     @recipe.member = current_member
     #list_storageのバリデーションチェックをして、問題がなければ保存
-    if is_list_storage_valid && @recipe.save
+    # if is_list_storage_valid && @recipe.save
+    if first_list_id.present? && @recipe.save
       redirect_to recipe_path(@recipe)
     else
       # 保存に失敗した場合、エラーメッセージを表示
       @recipe.valid?
       @recipe.errors.delete(:list_storages) if @recipe.errors.has_key?(:list_storages)
-      @recipe.errors.add(:base, 'てすと。食材を登録して下さい') if recipe_params[:list_storages_attributes].blank? || first_list_id.blank?
+      @recipe.errors.add(:base, '食材を登録して下さい') if recipe_params[:list_storages_attributes].blank? || first_list_id.blank?
       render :new
     end
   end
+
 
   def edit
     @recipe = Recipe.find(params[:id])
