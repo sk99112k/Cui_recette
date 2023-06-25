a9f7decd0a76b89642fd33a3216c94b68bb408ec
sk99112k <129582261+sk99112k@users.noreply.github.com>
2023-06-25 07:24:19 +0000

    インデント修正

--- app/controllers/public/lists_controller.rb
+++ app/controllers/public/lists_controller.rb
@@ -1,5 +1,4 @@
 class Public::ListsController < Public::ApplicationController
-  # before_action :ensure_guest_user, only: [:edit]
 
   def new
     @list = List.new
@@ -51,11 +50,5 @@ class Public::ListsController < Public::ApplicationController
   def list_params
     params.require(:list).permit(:name, :name_kana, :supplier, :price, :lot, :unit)
   end
-
-  # def ensure_guest_user
-  #   if  current_member.name == "guestuser"
-  #     redirect_to lists_path, notice: 'ゲストユーザーは編集画面へ遷移できません。'
-  #   end
-  # end
 
 end
