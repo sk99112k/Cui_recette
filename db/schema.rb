1a043260936196c0048838c91aa568bd30e8b910
sk99112 <129582261+sk99112k@users.noreply.github.com>
2023-09-12 18:54:27 +0900

    開発環境変更

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2023_07_11_014820) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2023_07_11_014820) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -87,7 +87,7 @@ ActiveRecord::Schema.define(version: 2023_07_11_014820) do
   create_table "list_storages", force: :cascade do |t|
     t.integer "list_id", null: false
     t.integer "recipe_id", null: false
-    t.decimal "quantity", null: false
+    t.decimal "quantity", precision: 8, scale: 2, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -96,9 +96,9 @@ ActiveRecord::Schema.define(version: 2023_07_11_014820) do
     t.string "name", null: false
     t.string "name_kana", null: false
     t.integer "price", null: false
-    t.decimal "kg_price", null: false
     t.string "supplier", null: false
-    t.decimal "lot", null: false
+    t.decimal "kg_price", precision: 8, scale: 2, null: false
+    t.decimal "lot", precision: 8, scale: 3, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
